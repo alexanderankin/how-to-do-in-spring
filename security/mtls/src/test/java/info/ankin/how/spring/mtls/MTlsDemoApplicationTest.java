64db733f7709a21a576d57d8cab736b250f31eb8
David Ankin <daveankin@gmail.com>
2023-02-27 17:15:45 -0500

    show that it is secure

--- security/mtls/src/test/java/info/ankin/how/spring/mtls/MTlsDemoApplicationTest.java
+++ security/mtls/src/test/java/info/ankin/how/spring/mtls/MTlsDemoApplicationTest.java
@@ -1,5 +1,7 @@
 package info.ankin.how.spring.mtls;
 
+import com.rabbitmq.client.TrustEverythingTrustManager;
+import io.netty.handler.codec.DecoderException;
 import io.netty.handler.ssl.SslContext;
 import io.netty.handler.ssl.SslContextBuilder;
 import lombok.SneakyThrows;
@@ -10,23 +12,33 @@ import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWeb
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.web.reactive.function.client.WebClientRequestException;
 import reactor.netty.http.client.HttpClient;
+import reactor.netty.tcp.SslProvider;
 
 import java.net.URI;
 import javax.net.ssl.KeyManagerFactory;
+import javax.net.ssl.SSLHandshakeException;
 import javax.net.ssl.TrustManagerFactory;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureWebTestClient(timeout = "PT24H")
 class MTlsDemoApplicationTest {
+    @Value("${local.server.port}") int port;
     WebTestClient webTestClient;
 
     @SneakyThrows
+    private String url() {
+        return new URI("https", null, "localhost", port, null, null, null).toString();
+    }
+
+    @SneakyThrows
     @Autowired
-    public void setWebTestClient(@Value("${local.server.port}") int port) {
+    public void setWebTestClient() {
         var weSend = KeyManagerFactory.getInstance("SunX509");
         weSend.init(SecurityConfig.NettyCustomizer.keyStore("/certs/localhost.crt"), new char[0]);
 
@@ -43,7 +55,7 @@ class MTlsDemoApplicationTest {
                         HttpClient.create()
                                 .secure(spec -> spec.sslContext(sslContext))
                 ))
-                .baseUrl(new URI("https", null, "localhost", port, null, null, null).toString())
+                .baseUrl(url())
                 .build();
     }
 
@@ -52,6 +64,39 @@ class MTlsDemoApplicationTest {
         var result = webTestClient.get().uri("/").exchange().expectBody(String.class).returnResult();
         System.out.println(result);
         assertThat(result.getResponseBody(), is("world"));
+    }
+
+    @SneakyThrows
+    @Test
+    void test_failsWithSystemCerts() {
+        WebTestClient webTestClient = WebTestClient.bindToServer(new ReactorClientHttpConnector(
+                HttpClient.create()
+                        .secure(SslProvider.defaultClientProvider())
+                ))
+                .baseUrl(url())
+                .build();
+
+        WebClientRequestException exception = assertThrows(WebClientRequestException.class, () -> webTestClient.get().uri("/").exchange());
+        assertThat(exception.getMessage(), startsWith("PKIX path building failed"));
+        assertThat(exception.getCause(), instanceOf(SSLHandshakeException.class));
+    }
+
+    @SneakyThrows
+    @Test
+    void test_failsWithArbitraryClientCerts() {
+        SslContext sslContext = SslContextBuilder.forClient()
+                .trustManager(new TrustEverythingTrustManager())
+                .build();
+        WebTestClient webTestClient = WebTestClient.bindToServer(new ReactorClientHttpConnector(
+                HttpClient.create()
+                        .secure(spec -> spec.sslContext(sslContext))
+                ))
+                .baseUrl(url())
+                .build();
+
+        WebClientRequestException exception = assertThrows(WebClientRequestException.class, () -> webTestClient.get().uri("/").exchange());
+        assertThat(exception.getMessage(), containsString("Received fatal alert: bad_certificate"));
+        assertThat(exception.getCause(), instanceOf(DecoderException.class));
     }
 
 }
