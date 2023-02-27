44b37731c7b2d462798f48a7bdd8382a38fe2cce
David Ankin <daveankin@gmail.com>
2023-02-26 21:28:13 -0500

    add basic xjc/jaxb example

---
+++ parsing/jaxb/src/main/java/info/ankin/how/parsing/jaxb/JAXBDemoApplication.java
@@ -0,0 +1,21 @@
+package info.ankin.how.parsing.jaxb;
+
+import info.ankin.how.parsing.jaxb.generated.ShipOrder;
+import lombok.SneakyThrows;
+import org.xmlunit.builder.Input;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import javax.xml.bind.JAXBContext;
+
+public class JAXBDemoApplication {
+    @SneakyThrows
+    public static void main(String[] args) {
+        String s = Files.readString(Path.of(JAXBDemoApplication.class.getResource("/jaxb-demo/documents/ship-order.xml").toURI()));
+
+        JAXBContext jaxbContext = JAXBContext.newInstance(ShipOrder.class);
+        ShipOrder shipOrder = (ShipOrder) jaxbContext.createUnmarshaller().unmarshal(Input.fromString(s).build());
+
+        System.out.println(shipOrder);
+    }
+}
