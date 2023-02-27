c4f7006337a4d434694184c90fe0db7d9f3ce253
David Ankin <daveankin@gmail.com>
2023-02-27 16:53:50 -0500

    show how to use mtls in spring

--- security/mtls/src/main/java/info/ankin/how/spring/mtls/SecurityConfig.java
+++ security/mtls/src/main/java/info/ankin/how/spring/mtls/SecurityConfig.java
@@ -1,9 +1,25 @@
 package info.ankin.how.spring.mtls;
 
+import lombok.SneakyThrows;
+import org.bouncycastle.asn1.pkcs.PrivateKeyInfo;
+import org.bouncycastle.openssl.PEMParser;
+import org.bouncycastle.openssl.jcajce.JcaPEMKeyConverter;
+import org.springframework.boot.web.embedded.netty.NettyReactiveWebServerFactory;
+import org.springframework.boot.web.server.Ssl;
+import org.springframework.boot.web.server.SslStoreProvider;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.stereotype.Component;
+
+import java.io.InputStreamReader;
+import java.security.KeyStore;
+import java.security.PrivateKey;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateFactory;
+import java.util.Objects;
 
 @Configuration
 public class SecurityConfig {
@@ -12,5 +28,68 @@ public class SecurityConfig {
         httpSecurity.csrf().disable();
         httpSecurity.authorizeExchange().anyExchange().permitAll();
         return httpSecurity.build();
+    }
+
+    @Component
+    public static class NettyCustomizer
+            implements WebServerFactoryCustomizer<NettyReactiveWebServerFactory> {
+
+        @Override
+        public void customize(NettyReactiveWebServerFactory factory) {
+            Ssl ssl = new Ssl();
+            ssl.setEnabled(true);
+            // require client sending their cert
+            ssl.setClientAuth(Ssl.ClientAuth.NEED);
+            factory.setSslStoreProvider(new SslStoreProvider() {
+                @SneakyThrows
+                @Override
+                public KeyStore getKeyStore() {
+                    // not sure why this has to be here
+                    return clientKeyStore();
+                }
+
+                @Override
+                public KeyStore getTrustStore() {
+                    return serverKeyStore();
+                }
+
+                @Override
+                public String getKeyPassword() {
+                    // https://stackoverflow.com/a/72580366
+                    return "";
+                }
+            });
+            factory.setSsl(ssl);
+        }
+
+        private KeyStore serverKeyStore() {
+            return keyStore("/certs/rootCA.crt");
+        }
+
+        private KeyStore clientKeyStore() {
+            return keyStore("/certs/localhost.crt");
+        }
+
+        @SneakyThrows
+        public static KeyStore keyStore(String name) {
+            // set up the store
+            KeyStore keyStore = KeyStore.getInstance("JKS");
+            keyStore.load(null);
+
+            // parse the x509 and save it into store
+            Certificate cert = CertificateFactory.getInstance("X.509")
+                    .generateCertificates(SecurityConfig.class.getResourceAsStream(name))
+                    .iterator().next();
+            keyStore.setCertificateEntry("cert", cert);
+
+            // parse the key and save it into the store
+            PEMParser pemParser = new PEMParser(new InputStreamReader(Objects.requireNonNull(
+                    SecurityConfig.class.getResourceAsStream(name.replace(".crt", ".key")))));
+            PrivateKey privateKey = new JcaPEMKeyConverter()
+                    .getPrivateKey((PrivateKeyInfo) pemParser.readObject());
+            keyStore.setKeyEntry("cert", privateKey, new char[0], new Certificate[]{cert});
+
+            return keyStore;
+        }
     }
 }
--- security/mtls/src/test/java/info/ankin/how/spring/mtls/MTlsDemoApplicationTest.java
+++ security/mtls/src/test/java/info/ankin/how/spring/mtls/MTlsDemoApplicationTest.java
@@ -1,10 +1,20 @@
 package info.ankin.how.spring.mtls;
 
+import io.netty.handler.ssl.SslContext;
+import io.netty.handler.ssl.SslContextBuilder;
+import lombok.SneakyThrows;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.netty.http.client.HttpClient;
+
+import java.net.URI;
+import javax.net.ssl.KeyManagerFactory;
+import javax.net.ssl.TrustManagerFactory;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
@@ -12,12 +22,34 @@ import static org.hamcrest.Matchers.is;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureWebTestClient(timeout = "PT24H")
 class MTlsDemoApplicationTest {
-    @Autowired
     WebTestClient webTestClient;
+
+    @SneakyThrows
+    @Autowired
+    public void setWebTestClient(@Value("${local.server.port}") int port) {
+        var weSend = KeyManagerFactory.getInstance("SunX509");
+        weSend.init(SecurityConfig.NettyCustomizer.keyStore("/certs/localhost.crt"), new char[0]);
+
+        var weCheckAgainst = TrustManagerFactory.getInstance("SunX509");
+        weCheckAgainst.init(SecurityConfig.NettyCustomizer.keyStore("/certs/rootCA.crt"));
+
+        SslContext sslContext = SslContextBuilder.forClient()
+                .keyManager(weSend)
+                .trustManager(weCheckAgainst)
+                .build();
+
+        this.webTestClient = WebTestClient
+                .bindToServer(new ReactorClientHttpConnector(
+                        HttpClient.create()
+                                .secure(spec -> spec.sslContext(sslContext))
+                ))
+                .baseUrl(new URI("https", null, "localhost", port, null, null, null).toString())
+                .build();
+    }
 
     @Test
     void test_helloWorld() {
-        var result = webTestClient.get().exchange().expectBody(String.class).returnResult();
+        var result = webTestClient.get().uri("/").exchange().expectBody(String.class).returnResult();
         System.out.println(result);
         assertThat(result.getResponseBody(), is("world"));
     }
