8dc9a2ab6c41b5f7da429f7ab48d48664c7a3d2e
Alex A <alexanderankin@gmail.com>
2023-07-08 11:40:29 -0400

    handle whitespace, simplify strings

--- experiment/antlr/a1/src/main/antlr/MyLexer.g4
+++ experiment/antlr/a1/src/main/antlr/MyLexer.g4
@@ -1,9 +1,15 @@
 lexer grammar MyLexer;
 
+WHITESPACE: [\p{WHITE_SPACE}];
 SEMI_COLON: ';';
-SINGLE_QUOTE: '\'';
-DOUBLE_QUOTE: '"';
+SINGLE_STRING: '\'' ~'\''* '\'';
+DOUBLE_STRING: '"' ~'"'* '"'; // todo implement ESCAPED_VALUE
+TICK_STRING: '`' ~'`'* '`';
 
 PACKAGE: 'package';
 
 IDENTIFIER: [a-zA-Z]+;
+
+IMPORT: 'import';
+PAREN_OPEN: '(';
+PAREN_CLOSE: ')';
--- experiment/antlr/a1/src/test/java/LexerTest.java
+++ experiment/antlr/a1/src/test/java/LexerTest.java
@@ -1,7 +1,9 @@
-import org.antlr.v4.runtime.CharStreams;
-import org.antlr.v4.runtime.Token;
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.atn.ATNConfigSet;
+import org.antlr.v4.runtime.dfa.DFA;
 import org.junit.jupiter.api.Test;
 
+import java.util.BitSet;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -11,11 +13,34 @@ class LexerTest {
     @Test
     void test_package() {
         MyLexer myLexer = new MyLexer(CharStreams.fromString("package 'abc'"));
-        List<String> expected = List.of("package", "'", "abc", "'");
+        myLexer.removeErrorListeners();
+        myLexer.addErrorListener(new StrictListener());
+        List<String> expected = List.of("package", " ", "'abc'");
         List<? extends Token> tokens = myLexer.getAllTokens();
         List<String> actual = tokens.stream().map(Token::getText).toList();
 
         assertEquals(expected, actual);
     }
 
+    private static class StrictListener implements ANTLRErrorListener {
+        @Override
+        public void syntaxError(Recognizer<?, ?> recognizer, Object offendingSymbol, int line, int charPositionInLine, String msg, RecognitionException e) {
+            throw new RuntimeException("syntaxError: " + msg);
+        }
+
+        @Override
+        public void reportAmbiguity(Parser recognizer, DFA dfa, int startIndex, int stopIndex, boolean exact, BitSet ambigAlts, ATNConfigSet configs) {
+            throw new RuntimeException("reportAmbiguity");
+        }
+
+        @Override
+        public void reportAttemptingFullContext(Parser recognizer, DFA dfa, int startIndex, int stopIndex, BitSet conflictingAlts, ATNConfigSet configs) {
+            throw new RuntimeException("reportAttemptingFullContext");
+        }
+
+        @Override
+        public void reportContextSensitivity(Parser recognizer, DFA dfa, int startIndex, int stopIndex, int prediction, ATNConfigSet configs) {
+            throw new RuntimeException("reportContextSensitivity");
+        }
+    }
 }
