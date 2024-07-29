164a68bea7eac2c6ff3aaf9c3bc992904c1cfb71
Dave Ankin <daveankin@gmail.com>
2024-07-29 09:10:54 -0400

    logout link

--- security/auth/oauth-in-memory/oauth-in-memory-client/src/main/java/org/example/oauth/learn/client/OAuthClientApplication.java
+++ security/auth/oauth-in-memory/oauth-in-memory-client/src/main/java/org/example/oauth/learn/client/OAuthClientApplication.java
@@ -2,8 +2,12 @@ package org.example.oauth.learn.client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @SpringBootApplication
 class OAuthClientApplication {
@@ -14,8 +18,15 @@ class OAuthClientApplication {
     @Controller
     static class AppController {
         @GetMapping("/")
-        String home() {
-            return "index.html";
+        ModelAndView home(@AuthenticationPrincipal OidcUser user) {
+            System.out.println(user);
+            return new ModelAndView("index", "user", new Model(user));
+        }
+
+        record Model(OidcUser oidcUser, String sub) {
+            Model(OidcUser oidcUser) {
+                this(oidcUser, oidcUser.getSubject());
+            }
         }
     }
 }
--- security/auth/oauth-in-memory/oauth-in-memory-client/src/main/java/org/example/oauth/learn/client/SecurityConfig.java
+++ security/auth/oauth-in-memory/oauth-in-memory-client/src/main/java/org/example/oauth/learn/client/SecurityConfig.java
@@ -3,14 +3,40 @@ package org.example.oauth.learn.client;
 import lombok.SneakyThrows;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.lang.NonNull;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    //<editor-fold desc="webjars">
+    @Bean
+    WebMvcConfigurer customResourceConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addResourceHandlers(@NonNull ResourceHandlerRegistry registry) {
+                registry.addResourceHandler("/webjars/**")
+                        .addResourceLocations("classpath:/META-INF/resources/webjars/");
+            }
+        };
+    }
+
+    @SneakyThrows
+    @Bean
+    @Order(-100)
+    public SecurityFilterChain webJarsLol(HttpSecurity httpSecurity) {
+        httpSecurity.securityMatcher("/webjars/**");
+        httpSecurity.authorizeHttpRequests(h -> h.anyRequest().permitAll());
+        return httpSecurity.build();
+    }
+    //</editor-fold>
 
     @SneakyThrows
     @Bean
